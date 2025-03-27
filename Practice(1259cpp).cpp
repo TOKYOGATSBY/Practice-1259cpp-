7ab90871136459741701269954697602a0cbc201
Grigorii Kolbeev <gold_grigorii@mail.ru>
2025-03-27 11:09:30 +0300

    version 1.0

--- Practice(1259cpp).cpp
+++ Practice(1259cpp).cpp
@@ -1,6 +1,29 @@
 ﻿#include <iostream>
+using namespace std;
 
-int main()
-{
-    
+long long eulerPhi(long long n) {
+    long long result = n;
+    for (long long p = 2; p * p <= n; p++) {
+        if (n % p == 0) {
+            while (n % p == 0) {
+                n /= p;
+            }
+            result -= result / p;
+        }
+    }
+
+    if (n > 1) {
+        result -= result / n;
+    }
+    return result;
+}
+
+int main() {
+    long long N;
+    cin >> N;
+
+    long long phiN = eulerPhi(N);
+    cout << phiN / 2 << endl;
+
+    return 0;
 }
